001ddf180e645ec5ed63deb42fd9ff2d9787bdfb
Judzuey <allexxdcsz@gmail.com>
2024-09-20 20:16:29 -0600

    Día 1: Variables y Tipos de Datos

---
+++ Día1_Programa1/CalculoAreaTriangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoAreaTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la base del triángulo:");
+            double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingresa la altura del triángulo:");
+            double alturaTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            double area = (baseTriangulo * alturaTriangulo) / 2;
+            Console.WriteLine($"El área del triángulo es: {area}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Día1_Programa1/nombreEdadAltura/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Día1_Programa1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu Nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu Edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingresa tu altura en metros (ej. 1.75):");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Hola {nombre}, tienes {edad} años y mides {altura} metros.");
+            Console.ReadLine();
+        }
+    }
+}
